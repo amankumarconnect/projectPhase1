10609c6bdc6f5ba4adc524d6cdb7929e683ad7b5
Aman Kumar <aman.kumar.connect@gmail.com>
2025-02-04 16:10:49 +0530

    add code for quote.js

---
+++ js/quote.js
@@ -0,0 +1,14 @@
+async function getData(url){
+    try {
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const json = await response.json();
+        console.log(json.content);
+        console.log(json.authorSlug);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+getData("https://api.quotable.io/random");
